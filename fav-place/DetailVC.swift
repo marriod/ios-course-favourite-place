e515b8b5189d60be5ef9d866fa96785d00aeb9ca
Dean Marriott <deanmarriott@Deans-Mac-mini.local>
2016-01-11 16:35:48 +0000

    Committing all files

--- fav-place/DetailVC.swift
+++ fav-place/DetailVC.swift
@@ -1,31 +1,28 @@
 //
-//  ViewController.swift
+//  DetailVC.swift
 //  fav-place
 //
 //   Created by Dean Marriott on 11/1/16 as part of the ios developer course.
 //  Copyright © 2016 gizmu. All rights reserved.
 //
 
-let SHADOW_COLOR: CGFloat = 157.0 / 255.0
-
 import UIKit
 
-class ViewController: UIViewController {
+class DetailVC: UIViewController {
 
-    @IBOutlet weak var moreBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        moreBtn.layer.cornerRadius = 2.0
 
-        
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
-
